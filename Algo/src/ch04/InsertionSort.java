f5a456b17e4f096fe0dfe4330ad036d989ff995f
buddy <81221429+taehwan0@users.noreply.github.com>
2022-09-22 21:51:13 +0900

    ch04-InsertionSort

---
+++ Algo/src/ch04/InsertionSort.java
@@ -0,0 +1,29 @@
+package src.ch04;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] array = {9, 5, 25, 70, 60, 30, 1, 8};
+        System.out.println("정렬 전 배열 : " + Arrays.toString(array));
+        insertionSort(array);
+        System.out.println("정렬 후 배열 : " + Arrays.toString(array));
+    }
+
+    public static void insertionSort(int[] array) {
+        // i == 정렬을 시작할 인덱스
+        // j == 정렬된 배열의 마지막 인덱스
+        for(int i = 1; i < array.length; i++) {
+            int j = i - 1;
+            int element = array[i];
+
+            while(j >= 0 && array[j] >= element) {
+                // 정렬된 인덱스가 더 크면 한칸 뒤로 저장하고 인덱스를 앞으로 이동
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = element;
+        }
+    }
+}
