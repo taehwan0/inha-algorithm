9dcf3ceb35f0c613f53ad0e35b22d2fc7720e184
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-13 22:00:44 +0900

    ch06: CutRodDP

---
+++ Algo/src/ch06/CutRod.java
@@ -0,0 +1,32 @@
+package src.ch06;
+
+/**
+ * Dynamic Programming Sample
+ */
+public class CutRod {
+
+    public static void main(String[] args) {
+        CutRod cutRod = new CutRod();
+        int[] price = {2, 5, 9, 10};
+
+        int maxPrice = cutRod.cutRodDP(price, price.length);
+        System.out.println("최대 금액: " + maxPrice);
+    }
+
+    public int cutRodDP(int[] price, int size) {
+        int[] maxSell = new int[size + 1];
+
+        maxSell[0] = 0;
+
+        for (int i = 1; i <= size; i++) {
+            int maxValue = 0;
+
+            for (int j = 1; j <= i ; j++) {
+                maxValue = Math.max(maxValue, price[j - 1] + maxSell[i - j]);
+            }
+            maxSell[i] = maxValue;
+        }
+
+        return maxSell[size];
+    }
+}
